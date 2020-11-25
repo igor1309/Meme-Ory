552ac97c1fb3dc0c14321d1e8c4f825f11984aa6
Igor Malyarov <igor1309@gmail.com>
2020-11-25 09:52:33 +0300

    migrate existing Core Data to Shared App Group

--- Meme-Ory/Core Data Stack/Persistence.swift
+++ Meme-Ory/Core Data Stack/Persistence.swift
@@ -13,18 +13,29 @@ struct PersistenceController {
     let container: NSPersistentCloudKitContainer
     
     init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Meme_Ory")
+    
         let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.photoigor.Meme-Ory")!
         let storeURL = containerURL.appendingPathComponent("Meme_Ory.sqlite")
-        let storeDescription = NSPersistentStoreDescription(url: storeURL)
-
-        container = NSPersistentCloudKitContainer(name: "Meme_Ory")
-        container.persistentStoreDescriptions = [storeDescription]
         
+        /// How to migrate existing Core Data to Shared App Group for use in extension? - Stack Overflow
+        /// https://stackoverflow.com/a/57020353/11793043
+        var defaultURL: URL?
+        if let storeDescription = container.persistentStoreDescriptions.first,
+           let url = storeDescription.url {
+            defaultURL = FileManager.default.fileExists(atPath: url.path) ? url : nil
+        }
+        
+        if defaultURL == nil {
+            let storeDescription = NSPersistentStoreDescription(url: storeURL)
+            container.persistentStoreDescriptions = [storeDescription]
+        }
+
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
         
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { [unowned container] (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -39,10 +50,28 @@ struct PersistenceController {
                  */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
+            
+            // migration
+            if !inMemory, let url = defaultURL, url.absoluteString != storeURL.absoluteString {
+                let coordinator = container.persistentStoreCoordinator
+                if let oldStore = coordinator.persistentStore(for: url) {
+                    do {
+                        try coordinator.migratePersistentStore(oldStore, to: storeURL, options: nil, withType: NSSQLiteStoreType)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                    
+                    // delete old store
+                    let fileCoordinator = NSFileCoordinator(filePresenter: nil)
+                    fileCoordinator.coordinate(writingItemAt: url, options: .forDeleting, error: nil, byAccessor: { url in
+                        do {
+                            try FileManager.default.removeItem(at: url)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    })
+                }
+            }
         })
     }
-    
-    //    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
-    //        (try? container.viewContext.count(for: fetchRequest)) ?? 0
-    //    }
 }
