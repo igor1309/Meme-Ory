c0c60d231b18c5e70eb26068ea107ad920017e8d
Igor Malyarov <igor1309@gmail.com>
2020-12-01 02:43:42 +0300

    list options menu refactoring to separate view

---
+++ Meme-Ory/Story Views/ListOptionsMenu.swift
@@ -0,0 +1,140 @@
+//
+//  ListOptionsMenu.swift
+//  Meme-Ory
+//
+//  Created by Igor Malyarov on 01.12.2020.
+//
+
+import SwiftUI
+
+struct ListOptionsMenu: View {
+    @Environment(\.managedObjectContext) private var context
+    
+    @EnvironmentObject private var filter: Filter
+    
+    @State private var showingListOptions = false
+    
+    var body: some View {
+        Menu {
+            Section {
+                showOptionsButton()
+            }
+            /// reset filter by tag(s)
+            resetFilterByTagSection()
+            /// list limit
+            Section {
+                listLimitButton()
+            }
+            Section {
+                Menu {
+                    Picker("Reminders", selection: $filter.remindersFilter) {
+                        ForEach(Filter.RemindersFilterOptions.allCases, id: \.self) { option in
+                            Label(option.rawValue, systemImage: option.icon)
+                                .tag(option)
+                        }
+                    }
+                } label: {
+                    Label(filter.remindersFilter.rawValue, systemImage: "chevron.right")
+                }
+            }
+            Section {
+                Menu {
+                    Picker("Favorites", selection: $filter.favoritesFilter) {
+                        ForEach(Filter.FavoritesFilterOptions.allCases, id: \.self) { option in
+                            Label(option.rawValue, systemImage: option.icon)
+                                .tag(option)
+                        }
+                    }
+                } label: {
+                    Label(filter.favoritesFilter.rawValue, systemImage: "chevron.right")
+                }
+            }
+            /// toggle sort order
+            sortOrderButton()
+            /// change item to sort by
+            Picker("Sort by", selection: $filter.itemToSortBy) {
+                ForEach(Filter.SortByOptions.allCases, id: \.self) { option in
+                    Label(option.rawValue, systemImage: option.icon)
+                        .tag(option)
+                }
+            }
+            //changeItemToSortByButton()
+            /// set list limit (number of stories showing)
+        } label: {
+            Image(systemName: "slider.horizontal.3")
+                .labelStyle(IconOnlyLabelStyle())
+        }
+        .accentColor(filter.isActive ? Color(UIColor.systemOrange) : Color(UIColor.systemBlue))
+        .sheet(isPresented: $showingListOptions) {
+            ListOptionView()
+                .environment(\.managedObjectContext, context)
+                .environmentObject(filter)
+        }
+    }
+    private func showOptionsButton() -> some View {
+        Button {
+            let haptics = Haptics()
+            haptics.feedback()
+            
+            withAnimation {
+                showingListOptions = true
+            }
+        } label: {
+            Label("List Options", systemImage: "slider.horizontal.3")
+                .padding([.vertical, .trailing])
+        }
+    }
+    
+    @ViewBuilder
+    private func resetFilterByTagSection() -> some View {
+        if filter.isTagFilterActive {
+            Section {
+                Button {
+                    let haptics = Haptics()
+                    haptics.feedback()
+                    
+                    withAnimation {
+                        filter.resetTags()
+                    }
+                } label: {
+                    Label("Reset Tags", systemImage: "tag.slash.fill")
+                }
+            }
+        } else {
+            EmptyView()
+        }
+    }
+    
+    private func listLimitButton() -> some View {
+        Button {
+            let haptics = Haptics()
+            haptics.feedback()
+            
+            withAnimation {
+                filter.isListLimited.toggle()
+            }
+        } label: {
+            Label(filter.isListLimited ? "Reset List Limit": "Set last Limit (\(filter.listLimit))",
+                  systemImage: filter.isListLimited ? "infinity" : "arrow.up.and.down")
+        }
+    }
+    
+    private func sortOrderButton() -> some View {
+        Button {
+            let haptics = Haptics()
+            haptics.feedback()
+            
+            withAnimation {
+                filter.areInIncreasingOrder.toggle()
+            }
+        } label: {
+            Label("Sort \(filter.areInIncreasingOrder ? "Descending": "Ascending")", systemImage: filter.areInIncreasingOrder ? "textformat" : "textformat.size")
+        }
+    }
+}
+
+struct ListOptionsMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        ListOptionsMenu()
+    }
+}
--- Meme-Ory/Story Views/StoryListView.swift
+++ Meme-Ory/Story Views/StoryListView.swift
@@ -38,7 +38,6 @@ struct StoryListView: View {
         _stories = FetchRequest(fetchRequest: fetchRequest)
     }
     
-    @State private var showingListOptions = false
     @State private var showingCreateSheet = false
     @State private var showingConfirmation = false
     @State private var showImportTextView = false
@@ -67,15 +66,11 @@ struct StoryListView: View {
         }
         .listStyle(InsetGroupedListStyle())
         .navigationBarTitle("Stories")
-        .navigationBarItems(
-            leading: HStack {
-                optionsMenu()
-            },
-            trailing: HStack {
-                importExportShareMenu()
-                createNewStoryButton()
-            }
-        )
+        .navigationBarItems(leading: ListOptionsMenu(),
+                            trailing: HStack {
+                                importExportShareMenu()
+                                createNewStoryButton()
+                            })
         .onChange(of: scenePhase, perform: handleScenePhase)
         .onOpenURL(perform: handleURL)
         .fileImporter(isPresented: $isImporting, allowedContentTypes: [UTType.json], onCompletion: handleFileImport)
@@ -185,8 +180,8 @@ struct StoryListView: View {
             }
         } label: {
             Label("Share Stories", systemImage: "square.and.arrow.up")
-            //                .imageScale(.large)
-            //                .foregroundColor(Color(UIColor.systemBlue))
+            // .imageScale(.large)
+            // .foregroundColor(Color(UIColor.systemBlue))
         }
     }
     
@@ -214,139 +209,6 @@ struct StoryListView: View {
             offsets.map { stories[$0] }.forEach(context.delete)
             
             context.saveContext()
-        }
-    }
-    
-    private func optionsMenu() -> some View {
-        Menu {
-            Section {
-                showOptionsButton()
-            }
-            /// reset filter by tag(s)
-            resetFilterByTagSection()
-            Section {
-                listLimitButton()
-            }
-            Section {
-                Menu {
-                    Picker("Reminders", selection: $filter.remindersFilter) {
-                        ForEach(Filter.RemindersFilterOptions.allCases, id: \.self) { option in
-                            Label(option.rawValue, systemImage: option.icon)
-                                .tag(option)
-                        }
-                    }
-                } label: {
-                    Label(filter.remindersFilter.rawValue, systemImage: "chevron.right")
-                }
-            }
-            Section {
-                Menu {
-                    Picker("Favorites", selection: $filter.favoritesFilter) {
-                        ForEach(Filter.FavoritesFilterOptions.allCases, id: \.self) { option in
-                            Label(option.rawValue, systemImage: option.icon)
-                                .tag(option)
-                        }
-                    }
-                } label: {
-                    Label(filter.favoritesFilter.rawValue, systemImage: "chevron.right")
-                }
-            }
-            /// toggle sort order
-            sortOrderButton()
-            /// change item to sort by
-            Picker("Sort by", selection: $filter.itemToSortBy) {
-                ForEach(Filter.SortByOptions.allCases, id: \.self) { option in
-                    Label(option.rawValue, systemImage: option.icon)
-                        .tag(option)
-                }
-            }
-            //changeItemToSortByButton()
-            /// set list limit (number of stories showing)
-        } label: {
-            Image(systemName: "slider.horizontal.3")
-                .labelStyle(IconOnlyLabelStyle())
-        }
-        .accentColor(filter.isActive ? Color(UIColor.systemOrange) : Color(UIColor.systemBlue))
-        .sheet(isPresented: $showingListOptions) {
-            ListOptionView()
-                .environment(\.managedObjectContext, context)
-                .environmentObject(filter)
-        }
-    }
-    
-    private func showOptionsButton() -> some View {
-        Button {
-            let haptics = Haptics()
-            haptics.feedback()
-            
-            withAnimation {
-                showingListOptions = true
-            }
-        } label: {
-            Label("List Options", systemImage: "slider.horizontal.3")
-                .padding([.vertical, .trailing])
-        }
-    }
-    
-    @ViewBuilder
-    private func changeItemToSortByButton() -> some View {
-        Button {
-            //  MARK: - FINISH THIS
-            //
-            switch filter.itemToSortBy {
-                case .timestamp: filter.itemToSortBy = .text
-                case .text:      filter.itemToSortBy = .timestamp
-            }
-        } label: {
-            Label(filter.itemToSortBy == .timestamp ? "Sort by Text" : "Sort by Date",
-                  systemImage: filter.itemToSortBy == .timestamp ? "text.cursor" : "calendar")
-        }
-    }
-    
-    private func sortOrderButton() -> some View {
-        Button {
-            let haptics = Haptics()
-            haptics.feedback()
-            
-            withAnimation {
-                filter.areInIncreasingOrder.toggle()
-            }
-        } label: {
-            Label("Sort \(filter.areInIncreasingOrder ? "Descending": "Ascending")", systemImage: filter.areInIncreasingOrder ? "textformat" : "textformat.size")
-        }
-    }
-    
-    private func listLimitButton() -> some View {
-        Button {
-            let haptics = Haptics()
-            haptics.feedback()
-            
-            withAnimation {
-                filter.isListLimited.toggle()
-            }
-        } label: {
-            Label(filter.isListLimited ? "Reset List Limit": "Set last Limit (\(filter.listLimit))",
-                  systemImage: filter.isListLimited ? "infinity" : "arrow.up.and.down")
-        }
-    }
-    
-    @ViewBuilder
-    private func resetFilterByTagSection() -> some View {
-        if filter.isTagFilterActive {
-            Section {
-                Button {
-                    let haptics = Haptics()
-                    haptics.feedback()
-                    
-                    withAnimation {
-                        filter.resetTags()
-                    }
-                } label: {
-                    Label("Reset Tags", systemImage: "tag.slash.fill")
-                }
-            }
-        } else {
-            EmptyView()
         }
     }
     
