c028119b32aec1938574de9a2eb52b94773a2c00
Igor Malyarov <igor1309@gmail.com>
2022-08-15 21:40:26 +0300

    add Swift Package with basic scaffolding

---
+++ swift-meme-ory/README.md
@@ -0,0 +1,3 @@
+# swift-meme-ory
+
+A description of this package.
---
+++ swift-meme-ory/Sources/swift-meme-ory/swift_meme_ory.swift
@@ -0,0 +1,6 @@
+public struct swift_meme_ory {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
