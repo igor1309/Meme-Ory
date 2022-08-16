ea9fb24b646aa1a747ba39118219bdc7f03b5dc4
Igor Malyarov <igor1309@gmail.com>
2022-08-16 21:13:26 +0300

    tweak script `test_app.sh`

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,16 +1,16 @@
-# do not forget to make file executable with `chmod +x the_file_name`
-
-
+# Run Xcode clean build and test according to provided parameters
+# do not forget to make this file executable with `chmod +x the_file_name`
 
 #!/bin/bash
 
+# `set -o pipefail` causes a pipeline (for example, `curl -s https://sipb.mit.edu/ | grep foo`) to produce a failure return code if any command errors. Normally, pipelines only return a failure if the last command errors. In combination with `set -e`, this will make your script exit if any command in a pipeline errors.
 set -eo pipefail
 
 xcodebuild clean build test \
            -project Meme-Ory.xcodeproj \
            -scheme "Meme-Ory" \
-           CODE_SIGN_IDENTITY="" \
-           CODE_SIGNING_REQUIRED=NO \
            -sdk iphonesimulator \
            -destination "platform=iOS Simulator,name=iPhone 13 Pro" \
+           CODE_SIGN_IDENTITY="" \
+           CODE_SIGNING_REQUIRED=NO \
            ONLY_ACTIVE_ARCH=YES | xcpretty
