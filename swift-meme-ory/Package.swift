7903ba00b1c236a62869592d85033162439c4c77
Igor <igor1309@gmail.com>
2023-06-12 12:27:48 +0300

    add scaffolding for `SingleStoryComponent`

--- swift-meme-ory/Package.swift
+++ swift-meme-ory/Package.swift
@@ -1,28 +1,52 @@
-// swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "swift-meme-ory",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "swift-meme-ory",
-            targets: ["swift-meme-ory"]),
+        .singleStoryComponent,
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "swift-meme-ory",
-            dependencies: []),
-        .testTarget(
-            name: "swift-meme-oryTests",
-            dependencies: ["swift-meme-ory"]),
+        .singleStoryComponent,
+        .singleStoryComponentTests,
     ]
 )
+
+private extension Product {
+    
+    static let singleStoryComponent = library(
+        name: .singleStoryComponent,
+        targets: [.singleStoryComponent])
+}
+
+private extension Target {
+    
+    static let singleStoryComponent = target(
+        name: .singleStoryComponent
+    )
+    static let singleStoryComponentTests =  testTarget(
+        name: .singleStoryComponentTests,
+        dependencies: [
+            .singleStoryComponent
+        ]
+    )
+}
+
+private extension Target.Dependency {
+    
+    static let singleStoryComponent = byName(
+        name: .singleStoryComponent
+    )
+}
+
+private extension String {
+    
+    static let singleStoryComponent = "SingleStoryComponent"
+    static let singleStoryComponentTests = "SingleStoryComponentTests"
+}
