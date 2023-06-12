7bf1cc70b6b5036c8a5b4f781384551f44307463
Igor <igor1309@gmail.com>
2023-06-12 13:00:57 +0300

    add `SingleStoryComponentPreview` scaffolding

---
+++ PreviewApps/SingleStoryComponentPreview/SingleStoryComponentPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleStoryComponentPreviewApp.swift
+//  SingleStoryComponentPreview
+//
+//  Created by Igor Malyarov on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SingleStoryComponentPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
